3bd18242c1c40c5b19b62213d59c01742f85aca3
daipei <daipei@celer.network>
2019-07-11 14:19:40 +0800

    🙈 add ignore file

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # testRecord
 - [post&update-match-history.md](./testRecord/post&update-match-history.md)
 # mermaid
-- [celer_solo_game_flow.md](./mermaid/celer_solo_game_flow.md)
-- [celer_solo_game_seq.md](./mermaid/celer_solo_game_seq.md)
-- [directory.py](./directory.py)
+<br><br>
+ > Note: 不要在仓库根目录直接增加文件，新增的文件放在对应的目录下面，每次新增文件后在命令行执行：python3 directory.py
--- directory.py
+++ directory.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/python3
 import os
 
 def generate(dir, depth, file): 
@@ -7,15 +8,22 @@ def generate(dir, depth, file):
         if path.startswith('.'):
             continue
         fullPath = os.path.join(dir, path) 
-        print(fullPath)
         if os.path.isdir(fullPath): 
             for i in range(0, depth):
                 file.write('#')
-            file.write(' ' + path + '\n')
+            title = ' ' + path + '\n'
+            print(title)
+            file.write(title)
             generate(fullPath, depth + 1, file) 
         else:
-            file.write('- [' + path + '](' + fullPath + ')' + '\n')
+            if not path.endswith('.md'):
+                continue
+            content = '- [' + path + '](' + fullPath + ')' + '\n'
+            print(content)
+            file.write(content)
 
 dir = './'
 with open('README.md', 'w') as README:
     generate(dir, 1, README)
+    README.write('<br><br>\n > Note: 不要在仓库根目录直接增加文件，新增的文件放在对应的目录下面，每次新增文件后在命令行执行：python3 directory.py')
+print('done!')
